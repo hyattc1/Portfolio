7bb8f104cbc6d6ff8d27216f0ec2a3c06d62309c
Connor Hyatt <connorhyatt1@gmail.com>
2024-11-23 05:07:17 -0500

    cursor tracking

--- js/main.js
+++ js/main.js
@@ -2,6 +2,59 @@
  	duration: 800,
  	easing: 'slide'
  });
+const circleElement = document.querySelector('.circle');
+
+// Create objects to track mouse position and custom cursor position
+const mouse = { x: 0, y: 0 }; // Track current mouse position
+const previousMouse = { x: 0, y: 0 }; // Store the previous mouse position
+const circle = { x: 0, y: 0 }; // Track the circle position
+
+// Initialize variables to track scaling and rotation
+let currentScale = 0; // Track current scale value
+let currentAngle = 0; // Track current angle value
+
+// Update mouse position on the 'mousemove' event
+window.addEventListener('mousemove', (e) => {
+	mouse.x = e.x;
+	mouse.y = e.y;
+});
+
+// Smoothing factor for cursor movement speed (0 = smoother, 1 = instant)
+const speed = 0.17;
+
+// Start animation
+const tick = () => {
+	// MOVE
+	circle.x += (mouse.x - circle.x) * speed;
+	circle.y += (mouse.y - circle.y) * speed;
+	const translateTransform = `translate(${circle.x}px, ${circle.y}px)`;
+
+	// SQUEEZE
+	const deltaMouseX = mouse.x - previousMouse.x;
+	const deltaMouseY = mouse.y - previousMouse.y;
+	previousMouse.x = mouse.x;
+	previousMouse.y = mouse.y;
+	const mouseVelocity = Math.min(Math.sqrt(deltaMouseX ** 2 + deltaMouseY ** 2) * 4, 150);
+	const scaleValue = (mouseVelocity / 150) * 0.5;
+	currentScale += (scaleValue - currentScale) * speed;
+	const scaleTransform = `scale(${1 + currentScale}, ${1 - currentScale})`;
+
+	// ROTATE
+	const angle = Math.atan2(deltaMouseY, deltaMouseX) * 180 / Math.PI;
+	if (mouseVelocity > 20) {
+		currentAngle = angle;
+	}
+	const rotateTransform = `rotate(${currentAngle}deg)`;
+
+	// Apply all transformations
+	circleElement.style.transform = `${translateTransform} ${rotateTransform} ${scaleTransform}`;
+
+	// Request the next frame
+	window.requestAnimationFrame(tick);
+};
+
+// Start the animation loop
+tick();
 
 (function($) {
 
